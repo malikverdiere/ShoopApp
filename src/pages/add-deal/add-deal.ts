7fdd46d711e9ca4403d3047195ad5002950b96d1
DESKTOP-04IIIN0\Malik <malik.verdiere@gmail.com>
2018-03-20 23:30:20 +0100

    qqq

--- src/pages/add-deal/add-deal.ts
+++ src/pages/add-deal/add-deal.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { PhotoLibrary } from '@ionic-native/photo-library';
 /**
  * Generated class for the AddDealPage page.
  *
@@ -15,11 +15,32 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AddDealPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
-  }
+  constructor(public navCtrl: NavController, public navParams: NavParams, private photoLibrary: PhotoLibrary) {
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad AddDealPage');
-  }
+    this.photoLibrary.requestAuthorization().then(() => {
+      this.photoLibrary.getLibrary().subscribe({
+        next: library => {
+          library.forEach(function (libraryItem) {
+            console.log(libraryItem.id);          // ID of the photo
+            console.log(libraryItem.photoURL);    // Cross-platform access to photo
+            console.log(libraryItem.thumbnailURL);// Cross-platform access to thumbnail
+            console.log(libraryItem.fileName);
+            console.log(libraryItem.width);
+            console.log(libraryItem.height);
+            console.log(libraryItem.creationDate);
+            console.log(libraryItem.latitude);
+            console.log(libraryItem.longitude);
+            console.log(libraryItem.albumIds);    // array of ids of appropriate AlbumItem, only of includeAlbumsData was used
+          });
+        },
+        error: err => { console.log('could not get photos'); },
+        complete: () => { console.log('done getting photos'); }
+      });
+    })
+      .catch(err => console.log('permissions weren\'t granted'));
 
+  }
+    ionViewDidLoad() {
+      console.log('ionViewDidLoad AddDealPage');
+    }
 }
